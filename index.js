3494e2b4033ca44238bce20e41e9721e7c440613
chandelierephrata <chandephrata@gmail.com>
2021-10-19 16:41:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function saturdayFun(){
+    console.log("This Saturday, I want to roller-skate")
+    console.log("This Saturday, I want to bathe my dog!")
+}
+function mondayWork(){
+    console.log("This Monday, I will go to the office")
+}
+function wrapAdjective(){
+    console.log ("You are %a dedicated programmer%!")
+}
